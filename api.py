ab79517a8c9cb6ae83435c010bdc665167131c9e
v889 <72309826+v889@users.noreply.github.com>
2022-09-16 14:34:57 +0530

    Initial commit

---
+++ api.py
@@ -0,0 +1,24 @@
+from hours import StudyHours
+import uvicorn
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+import numpy as  np
+app=FastAPI()
+pickle_in=open("C:/Users/DELL/Downloads/model.pkl","rb")
+classifier=pickle.load(pickle_in)
+@app.get('/{name}')
+def index():
+    return {"message":f'hello'}
+@app.post('/predict')
+def preduct(data:StudyHours):
+    data=data.dict()
+    print(data)
+    hours=data['hours']
+    predict=classifier.predict([[hours]])
+    print(predict[0][0])
+    ans=predict[0][0]
+    return { 'prediction':ans}
+
+if __name__== "__main__":
+    uvicorn.run(app,host='127.0.0.1',port=8000)
